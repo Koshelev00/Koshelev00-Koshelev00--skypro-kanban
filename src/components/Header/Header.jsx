c4aed9eee1f9667829dfeb8a81c4596c5f529f9a
Alexey Koshelev <koschelev00@mail.ru>
2025-03-03 20:42:58 +0300

    Сделал HW

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import '../../App.css'
+import PopUser from '../PopUser/PopUser'
+export default function Header() {
+    return (
+        <header className="header">
+        <div className="container">
+          <div className="header__block">
+            <div className="header__logo _show _light">
+              <a href="" target="_self"><img src={"/logo.png"} alt="logo"></img></a>
+            </div>
+            <div className="header__logo _dark">
+              <a href="" target="_self"><img src={"/logo_dark.png"} alt="logo"></img></a>
+            </div>
+            <nav className="header__nav">
+              <button className="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать новую задачу</a></button>
+              <PopUser/>
+            </nav>					
+          </div>
+        </div>			
+      </header>
+    )
+}
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,78 @@
+import '../../App.css'
+import Column from '../Column/Column'
+
+export default function Main() {
+    return (
+        <main className="main">
+    <div className="container">
+      
+      <div className="main__block">
+        <div className="main__content">
+
+        <Column/>
+        <Column/>
+        <Column/>
+        <Column/>
+        <Column/>
+        
+          {/* <div className="main__column column">
+            <div className="column__title">
+              <p>Без статуса</p>
+            </div>
+            <div className="cards">
+              <div className="cards__item">
+              <Card/>
+              </div>
+            </div>
+          </div>						
+          <div className="main__column">
+            <div className="column__title">
+              <p>Нужно сделать</p>
+            </div>
+            <div className="cards">
+              <div className="cards__item">
+              <Card/>
+              </div>
+            </div>
+          </div>
+          <div className="main__column">
+            <div className="column__title">
+              <p>В работе</p>
+            </div>
+            <div className="cards">
+              <div className="cards__item">
+              <Card/>
+              </div>
+          
+              
+            </div>
+          </div>
+          <div className="main__column">
+            <div className="column__title">
+              <p>Тестирование</p>
+            </div>
+            <div className="cards">
+              <div className="cards__item">
+              <Card/>
+              </div>
+            </div>
+          </div>
+          <div className="main__column">
+            <div className="column__title">
+              <p>Готово</p>
+            </div>
+            <div className="cards">
+              <div className="cards__item">
+              <Card/>
+              </div>
+            </div>
+          </div> */}
+      
+          
+        </div>
+      
+      </div>
+    </div>
+  </main>
+    )
+}
